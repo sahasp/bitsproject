7ceb7a02ddba6d684ad988c48971a8149aa87833
sahasp <sahas.parimoo@gmail.com>
2022-06-16 19:30:33 +0530

    update

--- js/script.js
+++ js/script.js
@@ -403,12 +403,10 @@ jQuery(".post-comt-box textarea").on("keydown", function(event) {
 
 // Get the button and container elements from HTML:
 const button = document.getElementById("theButton")
-const data = document.getElementById("info")
+const data = document.getElementById("replace-this")
 // Create an array of cars to send to the server:
-const cars = [
-	{ "make":"Porsche", "model":"911S" },
-	{ "make":"Mercedes-Benz", "model":"220SE" },
-	{ "make":"Jaguar","model": "Mark VII" }
+const textToTranslate = [
+	{ "data":document.getElementById("replace-this").innerText }
 ];
 // Create an event listener on the button element:
 button.onclick= function(){
@@ -421,7 +419,7 @@ button.onclick= function(){
                 'Accept': 'application/json'
             },
         // Strigify the payload into JSON:
-        body:JSON.stringify(cars)}).then(res=>{
+        body:JSON.stringify(textToTranslate)}).then(res=>{
                 if(res.ok){
                     return res.json()
                 }else{
@@ -430,9 +428,7 @@ button.onclick= function(){
             }).then(jsonResponse=>{
                 
                 // Iterate through the data with Map and write your rendering logic:
-                jsonResponse.map(Main=>            
-       Main.make==="Porsche"? data.innerHTML +="<p>"+ Main.make+" "+" is a good product":
-      data.innerHTML +="<p>"+ Main.make+" "+"is an average product" ) 
+                data.innerText = jsonResponse[0].data
 } 
 ).catch((err) => console.error(err)); } 
 
